2ef6e289596fee7a8328241c8aaa0b633c82da6b
Lucas Vieira <lvieirastos@gmail.com>
2021-01-14 06:30:49 -0300

    Implementado AppCompatDelegate Night Mode

--- app/src/main/java/com/example/atmconsultoria/MainActivity.java
+++ app/src/main/java/com/example/atmconsultoria/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
